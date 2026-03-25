437fea5d07f60966ba8b58a710ebf47868b00ee7
Mohd Afzal <afzalahmad@Mohds-MacBook-Pro.local>
2026-03-25 13:53:17 +0530

    fix: remove removeChild from RawScriptLoader

--- client/src/components/RawScriptLoader.js
+++ client/src/components/RawScriptLoader.js
@@ -6,13 +6,12 @@ export default function RawScriptLoader() {
   useEffect(() => {
     if (typeof window === "undefined") return;
 
-    // Load jQuery first
-    const jqueryScript = document.createElement("script");
-    jqueryScript.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
-    jqueryScript.onload = () => {
-      // Run plugin script only after jQuery is loaded
-      const script = document.createElement("script");
-      script.innerHTML = `
+    const loadPlugin = () => {
+      const jqueryScript = document.createElement("script");
+      jqueryScript.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+      jqueryScript.onload = () => {
+        const script = document.createElement("script");
+        script.innerHTML = `
 var eppathurl = window.location.origin + window.location.pathname;
 var eptagmanage = new XMLHttpRequest();
 eptagmanage.onreadystatechange = function() {
@@ -29,10 +28,18 @@ eptagmanage.onreadystatechange = function() {
 };
 eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmF1dG9zZW9wbHVnaW4uY29tL2FsbGhlYWRkYXRhP2VrZXk9ZS1BVVRPU0VPUExVR0lOOTQwOTY3MDI1NiZla2V5cGFzcz1pbFBIUlRWOXJhMzFKQkNOcXdUbGRBblNMd2s3RDE4b3JUVEImc2l0ZXVybD0=") + eppathurl);
 eptagmanage.send();
-      `;
-      document.body.appendChild(script);
+        `;
+        document.body.appendChild(script);
+      };
+      document.body.appendChild(jqueryScript);
     };
-    document.body.appendChild(jqueryScript);
+
+    // Delay until React has finished hydration and settled its DOM
+    if ("requestIdleCallback" in window) {
+      requestIdleCallback(loadPlugin);
+    } else {
+      setTimeout(loadPlugin, 500);
+    }
 
   }, []);
 
